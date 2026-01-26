fc4f66229e7eda7ad9781352b6f588e82b106d49
weewee231 <wee224225@gmail.com>
2026-01-26 12:34:11 +0500

    update config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,9 @@ rootProject.name = "order-platform"
 
 include(
     "common-libs",
+    "admin-service",
     "delivery-service",
     "order-service",
     "payment-service",
+    "user-service",
 )
